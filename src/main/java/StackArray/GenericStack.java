6ffb27aee088125053fe022f3697d25995eda804
faethflex <gfletchjr@gmail.com>
2017-05-18 17:43:52 -0400

    completed first two parts of the lab

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,6 +1,6 @@
 package StackArray;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
 
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
@@ -10,6 +10,34 @@ import java.util.Arrays;
 public class GenericStack<E> {
     private E[] elements;
 
-    public GenericStack() {
-    }
+    public GenericStack()
+	{
+		elements = (E[]) new Object[0];
+	}
+
+    public void push(E element)
+	{
+		E[] tempelements = (E[]) new Object[elements.length + 1];
+		int count = 0;
+		for (E i : elements)
+		{
+			tempelements[count] = i;
+			count++;
+		}
+		tempelements[elements.length] = element;
+		elements = tempelements;
+	}
+
+	public boolean isEmpty()
+	{
+		return elements.length == 0;
+	}
+
+	public String pop()
+	{
+		E element = elements[elements.length - 1];
+
+
+		return "";
+	}
 }
--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -6,11 +6,30 @@ import java.util.ArrayList;
  * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
-public class Stack<E> {
+public class Stack<E>
+{
     private ArrayList elements;
 
 
-    public Stack(){
-
+    public Stack()
+	{
+		elements = new ArrayList();
     }
+
+    public void push(E element)
+	{
+		elements.add(element);
+	}
+
+	public boolean isEmpty()
+	{
+		return elements.size() == 0;
+	}
+
+	public E pop()
+	{
+		E element = (E) elements.get(elements.size() - 1);
+		elements.remove(elements.size() - 1);
+		return element;
+	}
 }
